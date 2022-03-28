1699d1b21aaadc9c6f8e6eeaf941af13ed9047bc
Antonyhyson <102172525+Antonyhyson@users.noreply.github.com>
2022-03-28 11:34:03 +0530

    Update EIXFTOOL.py

--- EIXFTOOL.py
+++ EIXFTOOL.py
@@ -1,3 +1,18 @@
+# Disclaimer: This script is for educational purposes only.  
+# Do not use against any photos that you don't own or have authorization to test. 
+
+#!/usr/bin/env python3
+
+# Please note: 
+# This program is for .JPG and .TIFF format files. The program could be extended to support .HEIC, .PNG and other formats.
+# Installation and usage instructions:
+# 1. Install Pillow (Pillow will not work if you have PIL installed):
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --upgrade Pillow
+# 2. Add .jpg images downloaded from Flickr to subfolder ./images from where the script is stored. 
+# Note most social media sites strip exif data from uploaded photos.
+
+
 import os
 import sys
 from PIL import Image
